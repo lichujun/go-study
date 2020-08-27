e9fd9a943cb4b8bc121ac25b8ecba4052865c04a
joseph.li <joseph.li@yingzt.com>
2020-08-27 10:59:57 +0800

    add db demo

--- db/db_demo.go
+++ db/db_demo.go
@@ -6,25 +6,32 @@ import (
 	"github.com/gohouse/gorose/v2"
 )
 
-var err error
+var _ error
 var engin *gorose.Engin
 
 func init() {
-	// 全局初始化数据库,并复用
-	// 这里的engin需要全局保存,可以用全局变量,也可以用单例
-	// 配置&gorose.Config{}是单一数据库配置
-	// 如果配置读写分离集群,则使用&gorose.ConfigCluster{}
-	// mysql Dsn示例 "root:root@tcp(localhost:3306)/test?charset=utf8&parseTime=true"
-	engin, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: "root:root@tcp(localhost:3306)/test?charset=utf8&parseTime=true"})
+	engin, _ = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: "root:root@tcp(localhost:3306)/test?charset=utf8&parseTime=true"})
 }
 func DB() gorose.IOrm {
 	return engin.NewOrm()
 }
+
+type Test struct {
+	Age  int64 `json:"age"`  //
+	Time int64 `json:"time"` //
+}
+
+func (t *Test) TableName() string {
+	return "test"
+}
+
 func main() {
-	// 这里定义一个变量db, 是为了复用db对象, 可以在最后使用 db.LastSql() 获取最后执行的sql
-	// 如果不复用 db, 而是直接使用 DB(), 则会新建一个orm对象, 每一次都是全新的对象
-	// 所以复用 db, 一定要在当前会话周期内
 	db := DB()
-	res, err := db.Query("select version()")
-	fmt.Println(res, err)
+
+	t := Test{
+		Age:  15,
+		Time: 15,
+	}
+	db.Table(&t).Data(t).Where("age", "=", 14).Update()
+	fmt.Println(t)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,18 @@ go 1.13
 
 require github.com/gohouse/gorose/v2 v2.1.8
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/akkagao/table2go v0.0.0-20191205031357-5103fbb03fce // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/lib/pq v1.8.0 // indirect
+	github.com/magiconair/properties v1.8.2 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
+	github.com/spf13/afero v1.3.4 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6 // indirect
+	gopkg.in/ini.v1 v1.60.1 // indirect
+)
