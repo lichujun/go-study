5b6ea42704480a2039f848690aa3bff5b5963318
joseph.li <joseph.li@yingzt.com>
2020-11-27 17:56:16 +0800

    min edit distance、count prime

---
+++ algorithm/min_edit_distance_test.go
@@ -0,0 +1,10 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinEditDistance(t *testing.T) {
+	fmt.Println(MinEditDistance("bad", "apple"))
+}
---
+++ algorithm/prime_test.go
@@ -0,0 +1,10 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCountPrimes(t *testing.T) {
+	fmt.Println(CountPrimes(10))
+}
